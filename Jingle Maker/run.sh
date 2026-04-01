851c93efe2f93b032f9b2fa32f009faba1574ef3
Midnight <25554881+midnightgospel@users.noreply.github.com>
2026-04-01 18:30:25 -0400

    Delete Jingle Maker directory

--- Jingle Maker/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# 1. Enter the virtual environment
-source venv/bin/activate
-
-# 2. Force the environment to see Homebrew binaries
-export PATH="/opt/homebrew/bin:/usr/local/bin:$PATH"
-
-# 3. Run the app
-python main.py
